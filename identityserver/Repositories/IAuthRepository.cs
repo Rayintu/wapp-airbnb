adef247ad873086e930cdb111b200fd040799a9d
Ricky <rickybroers.rb@gmail.com>
2019-12-18 23:01:44 +0100

    push

--- identityserver/Repositories/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using identityserver.Models;
-
-namespace identityserver.Repositories {
-    public interface IAuthRepository {
-        User GetUserById(string id);
-        User GetUserByUsername(string username);
-        bool ValidatePassword(string username, string password);
-    }
-}
